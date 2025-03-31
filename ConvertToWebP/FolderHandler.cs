986931b4adb7efa4a14b994745422bf09125662d
Krasimir Dramaliev <krasipeace@abv.bg>
2025-03-31 14:24:00 +0300

    multiple files conversion - working

---
+++ ConvertToWebP/FolderHandler.cs
@@ -0,0 +1,38 @@
+﻿namespace ConvertToWebP;
+
+using System;
+
+using static ImageConverter;
+
+public static class FolderHandler
+{
+    public static void ConvertFolderImagesToWebp(string inputFolderPath, string outputFolderPath, int quality = 80)
+    {
+        if (!Directory.Exists(inputFolderPath))
+        {
+            Console.WriteLine($"Input folder '{inputFolderPath}' does not exist.");
+            return;
+        }
+
+        if (!Directory.Exists(outputFolderPath))
+        {
+            Directory.CreateDirectory(outputFolderPath);
+        }
+
+        string[] imageFiles =
+        [
+            .. Directory.GetFiles(inputFolderPath, "*.jpg"),
+            .. Directory.GetFiles(inputFolderPath, "*.jpeg"),
+            .. Directory.GetFiles(inputFolderPath, "*.png"),
+            .. Directory.GetFiles(inputFolderPath, "*.bmp"),
+        ];
+
+        foreach (string inputFile in imageFiles)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(inputFile);
+            string outputFile = Path.Combine(outputFolderPath, $"{fileName}.webp");
+
+            ConvertToWebp(inputFile, outputFile, quality);
+        }
+    }
+}
---
+++ ConvertToWebP/ImageConverter.cs
@@ -0,0 +1,25 @@
+﻿using ImageMagick;
+
+public static class ImageConverter
+{
+    public static void ConvertToWebp(string inputFilePath, string outputFilePath, int quality = 80)
+    {
+        try
+        {
+            using MagickImage image = new(inputFilePath);
+            image.Write(outputFilePath, MagickFormat.WebP);
+        }
+        catch (MagickException ex)
+        {
+            Console.WriteLine($"Error converting image: {ex.Message}");
+        }
+        catch (FileNotFoundException ex)
+        {
+            Console.WriteLine($"File not found: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+        }
+    }
+}
